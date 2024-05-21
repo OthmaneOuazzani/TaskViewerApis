3809cd5974d4114f0cae67d174769bd60a6bd76c
Othmane OUAZZANI TOUHAMI <othmaneouazzanit@gmail.com>
2024-05-21 14:39:07 +0100

    add errors class

---
+++ TaskViewerApis/Models/Errors.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskViewerApis.Models
+{
+    public class Errors
+    {
+        [Key]
+        public string PlmId { get; set; }
+        public string? Tokill { get; set; }
+        public string? Category { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Explanation { get; set; }
+        public string? Actors { get; set; }
+        public string? Workshop { get; set; }
+        public string? Url { get; set; }
+        public string? Contributors { get; set; }
+        public string? UseAsKpiFor { get; set; }
+        public string? LastTouch { get; set; }
+        public string? CorrectiveAction { get; set; }
+        public string? RelatedObject { get; set; }
+    }
+}
